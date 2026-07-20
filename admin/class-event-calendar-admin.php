79287e15afdc917556492425de1055104ee9a1e8
Charles Egana <charles@ideamaker.agency>
2026-07-20 16:31:32 +0800

    Initial commit

---
+++ admin/class-event-calendar-admin.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Event_Calendar
+ * @subpackage Event_Calendar/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Event_Calendar
+ * @subpackage Event_Calendar/admin
+ * @author     Your Name <email@example.com>
+ */
+class Event_Calendar_Admin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $event_calendar    The ID of this plugin.
+	 */
+	private $event_calendar;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $event_calendar       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $event_calendar, $version ) {
+
+		$this->event_calendar = $event_calendar;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Event_Calendar_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Event_Calendar_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->event_calendar, plugin_dir_url( __FILE__ ) . 'css/event-calendar-admin.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Event_Calendar_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Event_Calendar_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->event_calendar, plugin_dir_url( __FILE__ ) . 'js/event-calendar-admin.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
---
+++ includes/class-event-calendar-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Event_Calendar
+ * @subpackage Event_Calendar/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Event_Calendar
+ * @subpackage Event_Calendar/includes
+ * @author     Your Name <email@example.com>
+ */
+class Event_Calendar_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
